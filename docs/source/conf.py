97450532d86362e5ac306aa4654b2b5ed5e00c75
s307543138 <31691817+s307543138@users.noreply.github.com>
2025-11-13 14:32:53 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,15 @@
 
 # -- Project information
 
-project = 'TestDoc'
+project = 'RinasWeld Offline Manual Test'
 copyright = '2025, KPS'
 author = 'KPS'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
+
+html_title = "RinasWeld Offline Manual"
+html_short_title = "RinasWeld Manual"
 
 # -- General configuration
 
